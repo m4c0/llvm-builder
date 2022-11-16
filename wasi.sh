431284bde5099a9799468f8900cfc1281e7cc4fc
Eduardo Costa <m4c0@users.noreply.github.com>
2022-11-16 18:04:55 +0000

    more flags and some post-build file move
    This enables compilation of clang-rt (which requires some arcane
    knowledge to properly build) and allows usage of custom libc++ with wasi
    in a single sysroot

--- wasi.sh
+++ wasi.sh
@@ -2,28 +2,46 @@
 
 set -ex
 
+export CC=${LLVM}/bin/clang
+export CXX=${LLVM}/bin/clang++
+
 export INSTALL=`pwd`/prefix
 export SYSROOT=$INSTALL/sysroot
 
 make -C wasi-libc
 
-cmake -G Ninja -S llvm-project/runtimes -B llvm-project/build \
+cmake -G Ninja -S llvm-project/runtimes -B build/runtimes \
   -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;compiler-rt" \
+  -DLLVM_CONFIG_PATH=$LLVM/bin/llvm-config \
   -DCMAKE_C_COMPILER_WORKS=ON \
   -DCMAKE_CXX_COMPILER_WORKS=ON \
   -DCMAKE_TOOLCHAIN_FILE=`pwd`/wasi-toolchain.cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSROOT=$SYSROOT \
   -DCMAKE_INSTALL_PREFIX=$INSTALL \
-  -DUNIX:BOOL=ON \
-  -DLIBCXXABI_ENABLE_THREADS:BOOL=OFF \
-  -DLIBCXXABI_ENABLE_SHARED:BOOL=OFF \
-  -DLIBCXXABI_ENABLE_EXCEPTIONS:BOOL=OFF \
-  -DLIBCXXABI_HAS_PTHREAD_API:BOOL=OFF \
-  -DLIBCXX_HAS_MUSL_LIBC:BOOL=ON \
-  -DLIBCXX_ENABLE_SHARED:BOOL=OFF \
-  -DLIBCXX_ENABLE_THREADS:BOOL=OFF \
-  -DLIBCXX_ENABLE_FILESYSTEM:BOOL=OFF \
-  -DLIBCXX_ENABLE_EXCEPTIONS:BOOL=OFF 
-ninja -C llvm-project/build cxx cxxabi compiler-rt
-ninja -C llvm-project/build install-cxx install-cxxabi install-compiler-rt
+  -DUNIX=ON \
+  -DLIBCXXABI_ENABLE_THREADS=OFF \
+  -DLIBCXXABI_ENABLE_SHARED=OFF \
+  -DLIBCXXABI_ENABLE_EXCEPTIONS=OFF \
+  -DLIBCXXABI_HAS_PTHREAD_API=OFF \
+  -DLIBCXX_HAS_MUSL_LIBC=ON \
+  -DLIBCXX_ENABLE_SHARED=OFF \
+  -DLIBCXX_ENABLE_THREADS=OFF \
+  -DLIBCXX_ENABLE_FILESYSTEM=OFF \
+  -DLIBCXX_ENABLE_EXCEPTIONS=OFF \
+  -DCOMPILER_RT_BAREMETAL_BUILD=ON \
+  -DCOMPILER_RT_BUILD_BUILTINS=ON \
+  -DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
+  -DCOMPILER_RT_BUILD_MEMPROF=OFF \
+  -DCOMPILER_RT_BUILD_PROFILE=OFF \
+  -DCOMPILER_RT_BUILD_SANITIZERS=OFF \
+  -DCOMPILER_RT_BUILD_XRAY=OFF \
+  -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON \
+  -DCOMPILER_RT_OS_DIR=wasi
+ninja -C build/runtimes cxx cxxabi compiler-rt
+ninja -C build/runtimes install-cxx install-cxxabi install-compiler-rt
+
+pushd $INSTALL
+mv sysroot/include include/wasm32-wasi
+mv sysroot/lib/wasm32-wasi lib
+popd
